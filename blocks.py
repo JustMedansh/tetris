ee5cf7bea505dcad24642e88950f537b9ec6ff3b
Medansh <just.medansh@gmail.com>
2026-09-09 19:10:11 +0530

    done

--- blocks.py
+++ blocks.py
@@ -10,6 +10,7 @@ class Block:
         "S": [(1, 0), (2, 0), (0, 1), (1, 1)],
         "T": [(1, 0), (0, 1), (1, 1), (2, 1)],
         "Z": [(0, 0), (1, 0), (1, 1), (2, 1)],
+        'M': [*[(x, 0) for x in range(2)], *[(x, 1) for x in range(2)]],
     }
 
     COLORS = {
@@ -19,17 +20,30 @@ class Block:
         "S": "green",
         "Z": "red",
         "J": "blue",
-        "L": "white"
+        "L": "white",
     }
 
-    def __init__(self) -> None:
+    @staticmethod
+    def get_pivot(key):
+        if key == 'I':
+            return (1.5, 1.5)
+
+        elif key == 'O':
+            return None
+
+        else:
+            return (1, 1)
+
+    def __init__(self, board: Grid) -> None:
         """Initialize a block"""
         self.block_name = random.choice(['I', 'J', 'L', 'O', 'S', 'T', 'Z'])
         self.block_shape = self.BLOCK_SHAPES[self.block_name]
         self.color = self.COLORS[self.block_name]
-        self.dx = 0
+        self.dx = random.randint(0, 6)
         self.dy = 0
         self.symbol = '1'
+        self.pivot = self.get_pivot(self.block_name)
+        self.board = board
 
     def will_collide(self, grid: Grid) -> bool:
         """Return True if block will collide when moved down"""
@@ -60,3 +74,115 @@ class Block:
 
         self.dy += 1
         return True
+
+    def can_move(self, dx, grid: Grid):
+        for x, y in self.get_block_position():
+            new_x = x + dx
+
+            if new_x < 0 or new_x >= 10 or grid.get_cell(new_x, y).get_metadata('filled') is True:
+                return False
+
+        return True
+
+    def move(self, dir, value, grid):
+        dx = -value if dir == 'a' else value
+
+        if self.can_move(dx, grid):
+            self.dx += dx
+
+    def can_spawn(self, grid: Grid):
+        matching_pixels = [pixel for pixel in grid.get_all_cells(include_empty=False) if pixel.get_metadata('filled') is True and pixel.y == 0]
+        if len(matching_pixels) > 0:
+            return False
+        pixels = self.get_block_position()
+        for x, y in pixels:
+            if x < 0 or x >= grid.cols:
+                return False
+
+            if y < 0 or y >= grid.rows:
+                return False
+            
+            if grid.get_cell(x, y).get_metadata('filled') is True:
+                return False
+
+        return True
+
+    def rotate(self):
+        if not self.pivot:
+            return self
+        
+        pivotX, pivotY = self.pivot
+        
+        rotated = []
+        for pixelX, pixelY in self.block_shape:
+            new_x = pivotX - (pixelY - pivotY)
+            new_y = pivotY + (pixelX - pivotX)
+            rotated.append((new_x, new_y))
+
+
+        block = self.fix_rotate_collision(rotated)
+        if block is not None:
+            self.block_shape = block
+        return self
+
+    def get_position_local(self, shape):
+        moved_block = []
+        for x, y in shape:
+            moved_block.append((x + self.dx, y + self.dy))
+
+        return moved_block
+    
+    def fix_rotate_collision(self, rotated):
+        KICKS = [
+            (0, 0),
+            (-1, 0),
+            (1, 0),
+            (-2, 0),
+            (2, 0),
+            (0, -1),
+            (0, 1),
+        ]
+
+        for kick in KICKS:
+            onboard = self.get_position_local(rotated)
+            block = self.apply_kick(onboard, kick)
+            if not self.is_colliding(block):
+                return self.apply_kick(rotated, kick)
+
+    def is_colliding(self, block):
+        board = self.board
+        for pixelx, pixely in block:
+            try:
+                cell = board.get_cell(pixelx, pixely)
+                if cell.get_metadata('filled') is True:
+                    return True
+            except ValueError:
+                return True
+
+        return False
+
+    def apply_kick(self, block, kick):
+        applied = []
+        kickX, kickY = kick
+        for x, y in block:
+            applied.append((x + kickX, y + kickY))
+
+        return applied
+            
+
+class Pixel:
+    def __init__(self, color, x, y) -> None:
+            """Initialize a pixel"""
+            self.color = color
+            self.x = x
+            self.y = y
+
+    def line_clear(self, line_number):
+        """Return true if block is deleted"""
+        if line_number == self.y:
+            return True
+
+        if line_number > self.y:
+            self.y += 1
+
+        return False
--- main.py
+++ main.py
@@ -1,47 +1,119 @@
 from ttygrid import Grid, Cell
-from blocks import Block
+from blocks import Block, Pixel
 import time
+import sys
+from termcolor import colored
+from blessed import Terminal
 
+global_symbol = '█'
+score = 0
 
 def main():
+    term = Terminal()
     board = Grid(rows=20, cols=10, mode="custom")
-    blocks = []
+    pixels: list[Pixel] = [] 
 
-    for _ in range(11):
-        falling_block = Block()
-        while True:
-            time.sleep(0.02)
-            if not falling_block.fall(board):
-                break
+    with term.cbreak(), term.hidden_cursor():
+        for i in range(50):
+            # BEFORE FALLING BLOCK START
+            falling_block = Block(board)
+            m = 0
+            if not falling_block.can_spawn(board):
+                sys.exit(f"Game over!\nScore: {score}")
 
-            board.clear_term()
-            print_and_render(board, blocks, falling_block)
-        blocks.append(falling_block)
-    print_and_render(board, blocks, falling_block)
+            print("PASSED SPAWN:", falling_block.get_block_position())
+            # BEFORE FALLING BLOCK END
+            while True:
+                # FALLING BLOCK START
+                m += 1
+                key = term.inkey(timeout=0.02)
+                if key in ['a', 'd']:
+                    falling_block.move(key, 1, board)
+
+                if key == 's':
+                    if not falling_block.fall(board):
+                        break
+
+                if key == 'q':
+                    falling_block.rotate()
+
+                time.sleep(0.02)
+                if m % 3 == 0:
+                    if not falling_block.fall(board):
+                        break
+
+                board.clear_term()
+                print_and_render(board, pixels, falling_block)
+                # FALLING BLOCK END
+            # AFTER FALLING BLOCK START
+
+            pixels.extend(decompose(falling_block))
+            check_line_clear(board, pixels)
+            # AFTER FALLING BLOCK END
+            
+        pixels.extend(decompose(falling_block))
+        print_and_render(board, pixels)
 
 
-def print_and_render(board, blocks, falling):
+def check_line_clear(board: Grid, pixels: list[Pixel]):
+    global score
+    for i in range(20):
+            matching_pixels = [pixel for pixel in pixels if pixel.y == i]
+            if len(matching_pixels) == board.cols:
+                score += 1
+                for pixel in pixels.copy():
+                    if pixel.line_clear(i):
+                        pixels.remove(pixel)
+
+    return board, pixels
+
+
+def automated_line_clear(board, pixels):
+    for _ in range(19, 0, -1):
+        for pixel in pixels.copy():
+            if pixel.line_clear(19):
+                pixels.remove(pixel)
+        time.sleep(0.2)
+        print_and_render(board, pixels)
+
+def decompose(block: Block) -> list[Pixel]:
+    pixels = []
+    for px, py in block.get_block_position():
+        pixels.append(Pixel(block.color, px, py))
+
+    return pixels
+
+def print_and_render(board: Grid, blocks, falling=None):
     render_blocks(board, blocks)
-    render_one_block(board, falling)
+    if falling is not None:
+        render_one_block(board, falling)
 
     print('─'*board.cols, end="")
-    print(board)
+    # print(board.render(block_renderer))
+    print(board.render())
     print('─'*board.cols, end="")
 
     print()
 
-def render_blocks(grid: Grid, blocks: list[Block]) -> Grid:
+def block_renderer(cell: Cell):
+    if cell.symb == '█':
+        color = cell.get_metadata('color')
+        return colored('██', color)
+
+    else:
+        return ''
+
+def render_blocks(grid: Grid, pixels: list[Pixel]) -> Grid:
     grid.redraw_frame(grid.gen_cell_map(grid.rows, grid.cols, ' '))
 
-    for block in blocks:
-        for pixel_x, pixel_y in block.get_block_position():
-            grid.draw_cells(Cell(pixel_x, pixel_y, '█', {'color': block.color, 'filled': True}))
+    for pixel in pixels:
+        grid.draw_cells(Cell(pixel.x, pixel.y, global_symbol, {'color': pixel.color, 'filled': True}))
 
     return grid
 
 def render_one_block(grid: Grid, block: Block) -> Grid:
     for pixel_x, pixel_y in block.get_block_position():
-        grid.draw_cells(Cell(pixel_x, pixel_y, '█', {'color': block.color}))
+        grid.draw_cells(Cell(pixel_x, pixel_y, global_symbol, {'color': block.color}))
 
     return grid
 
